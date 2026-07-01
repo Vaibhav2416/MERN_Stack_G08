a35f5332e92b7dfad6edc513a97fc94aed418d5b
Vaibhav Sultane <vaibhavsultane1997@gmail.com>
2026-07-01 17:42:17 +0530

    day 2 updated with dom content

---
+++ phase_01_HTMLCSSJS/day_02/moviesdb.js
@@ -0,0 +1,37 @@
+
+// var, let, const
+
+var btn=document.getElementById("submit")
+
+btn.addEventListener("click",handleClick)
+
+function handleClick(){
+ var image=document.getElementById("url")
+ var title=document.getElementById("title")
+ var rating=document.getElementById("rating")
+
+ var inpImage=image.value
+ var inpTitle=title.value
+ var inpRating =rating.value
+ 
+ var movieDiv=document.getElementById("movieCards")
+ 
+ movieDiv.innerHTML+=`
+  <div class="card">
+            <img src="${inpImage}" alt="">
+            <p>Movie : ${inpTitle}</p>
+            <h3>Rating: ${inpRating}</h3>
+    </div>
+ `
+ image.value=""
+ title.value=""
+ rating.value=""
+ 
+}
+
+
+
+
+
+
+// document.querySelector("#submit")
